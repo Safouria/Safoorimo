e3b26fb65cfa2da64cff6737d513720ed8b88b78
Safoura <safari.safoora@gmail.com>
2024-11-26 17:27:14 +0330

    better

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
       </div>
       <div className="more-cards">
       <TitleCards title={"Top Rated"} category={"top_rated"}/>
-      <TitleCards title={"Only on Netflix"} category={"popular"}/>
+      <TitleCards title={"Only on Safoorimo"} category={"popular"}/>
       <TitleCards title={"Upcoming"} category={"upcoming"}/>
       <TitleCards title={"Top pics for You"} category={"now_playing"}/>
       </div>
